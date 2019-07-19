aeb6d8519f7b14781f8ee2ceb4bd3d361fdbfd90
bigstonny1 <43025240+bigstonny1@users.noreply.github.com>
2019-07-18 22:05:05 -0300

    Create config.php

---
+++ re/config.php
@@ -0,0 +1,21 @@
+<?php 
+ 
+  ////////////////////////////
+ // REDIRECIONAMENTO FINAL //
+////////////////////////////
+
+    //Caso você não tenha lido antes sobre a configuração dos redirecionamentos
+    //POR FAVOR, LEIA ATENTAMENTE O ARQUIVO "LEIA-ME.TXT" QUE SE ENCONTRA NA PASTA "Manual Script/Redirecionamentos/"
+    //Se você ja leu, por favor desconsidere.
+
+  //////////////////
+ // CONFIGURANDO //
+//////////////////
+
+    //Aqui você coloca o url do site onde está o Protetor
+    //Exemplo: http://www.meuprotetor.com/
+	$url_site_protetor = "https://eagon.work/"; 
+
+
+    // OBS: Coloque este arquivo em somente no site que será o ultimo a redirecionar antes do protetor, no caso, este aqui já vai redirecionar direto para o protetor!
+?>

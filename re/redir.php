d5205fde1fbf7f430845bdcac3645e117c132ae7
bigstonny1 <43025240+bigstonny1@users.noreply.github.com>
2019-07-18 22:06:26 -0300

    Create redir.php

---
+++ re/redir.php
@@ -0,0 +1,17 @@
+<?php 
+ include 'config.php';
+  //////////////////////////////
+ // POR FAVOR NÃO MECHA AQUI //
+//////////////////////////////
+
+?>
+	<!DOCTYPE html>
+	<html>
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta name="robots" content="noindex, nofollow">
+	<meta http-equiv="refresh" content="0;url=<?php echo $url_site_protetor ?>redir.php">
+	</head>
+	<body>
+	</body>
+	</html>
